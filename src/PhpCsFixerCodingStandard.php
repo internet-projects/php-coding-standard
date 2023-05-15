38513ba78ffb1c9bb024ac5733c7b1821d3c59ad
andrew <and.ivchenkov@gmail.com>
2023-05-15 23:04:56 +0300

    init

---
+++ src/PhpCsFixerCodingStandard.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace InternetProjects\PhpCodingStandard;
+
+use PhpCsFixer\ConfigInterface;
+
+final class PhpCsFixerCodingStandard
+{
+    public static function applyTo(ConfigInterface $config): void
+    {
+        $config
+            ->setRiskyAllowed(true)
+            ->setRules([
+                '@PHP82Migration' => true,
+                '@PhpCsFixer' => true,
+                '@PhpCsFixer:risky' => true,
+                'function_declaration' => ['closure_fn_spacing' => 'none'],
+                'concat_space' => ['spacing' => 'one'],
+                'cast_spaces' => ['space' => 'none'],
+                'yoda_style' => [
+                    'equal' => false,
+                    'identical' => false,
+                    'less_and_greater' => false,
+                    'always_move_variable' => false,
+                ],
+                'phpdoc_to_comment' => false,
+                'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+                'phpdoc_types_order' => ['sort_algorithm' => 'none', 'null_adjustment' => 'always_last'],
+                'increment_style' => ['style' => 'post'],
+                'native_function_invocation' => false,
+            ]);
+    }
+}

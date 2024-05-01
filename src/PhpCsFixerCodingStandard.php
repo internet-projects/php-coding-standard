d4c28313c7677a9e4c3f0e7cf99b120758e43b75
andrew <and.ivchenkov@gmail.com>
2024-05-01 23:08:22 +0300

    replace unpacking with array_merge as it's not working with string keys on PHP 7.4

--- src/PhpCsFixerCodingStandard.php
+++ src/PhpCsFixerCodingStandard.php
@@ -12,32 +12,34 @@ final class PhpCsFixerCodingStandard
     {
         $config
             ->setRiskyAllowed(true)
-            ->setRules([
-                '@PHP82Migration' => true,
-                '@PhpCsFixer' => true,
-                '@PhpCsFixer:risky' => true,
-                'function_declaration' => ['closure_fn_spacing' => 'none'],
-                'concat_space' => ['spacing' => 'one'],
-                'cast_spaces' => ['space' => 'none'],
-                'yoda_style' => [
-                    'equal' => false,
-                    'identical' => false,
-                    'less_and_greater' => false,
-                    'always_move_variable' => false,
+            ->setRules(array_merge(
+                [
+                    '@PHP82Migration' => true,
+                    '@PhpCsFixer' => true,
+                    '@PhpCsFixer:risky' => true,
+                    'function_declaration' => ['closure_fn_spacing' => 'none'],
+                    'concat_space' => ['spacing' => 'one'],
+                    'cast_spaces' => ['space' => 'none'],
+                    'yoda_style' => [
+                        'equal' => false,
+                        'identical' => false,
+                        'less_and_greater' => false,
+                        'always_move_variable' => false,
+                    ],
+                    'phpdoc_to_comment' => false,
+                    'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
+                    'phpdoc_types_order' => ['sort_algorithm' => 'none', 'null_adjustment' => 'always_last'],
+                    'increment_style' => ['style' => 'post'],
+                    'native_function_invocation' => false,
+                    'php_unit_internal_class' => false,
+                    'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
+                    'echo_tag_syntax' => ['format' => 'short', 'shorten_simple_statements_only' => true],
+                    'semicolon_after_instruction' => false,
+                    'date_time_immutable' => true,
+                    'ordered_types' => false,
+                    'fully_qualified_strict_types' => true,
                 ],
-                'phpdoc_to_comment' => false,
-                'multiline_whitespace_before_semicolons' => ['strategy' => 'no_multi_line'],
-                'phpdoc_types_order' => ['sort_algorithm' => 'none', 'null_adjustment' => 'always_last'],
-                'increment_style' => ['style' => 'post'],
-                'native_function_invocation' => false,
-                'php_unit_internal_class' => false,
-                'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
-                'echo_tag_syntax' => ['format' => 'short', 'shorten_simple_statements_only' => true],
-                'semicolon_after_instruction' => false,
-                'date_time_immutable' => true,
-                'ordered_types' => false,
-                'fully_qualified_strict_types' => true,
-                ...$customRules
-            ]);
+                $customRules
+            ));
     }
 }
